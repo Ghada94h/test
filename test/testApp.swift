ae19159926826fc2a1876284864342b91bb47efb
Ghada94h <ghada1415H@gmail.com>
2022-11-03 10:57:16 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ghadah Alamri on 09/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
